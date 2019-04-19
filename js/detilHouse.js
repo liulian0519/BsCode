210dba9f9d8f52229ef1a0d351ebbad668a6b69e
liulian0519 <1223342263@qq.com>
2019-04-19 15:18:12 +0800

    map

--- js/detilHouse.js
+++ js/detilHouse.js
@@ -1,7 +1,19 @@
 /**
- * created by liulian on 2019/04/08
+ * created by liulian on 2019/04/19
  */
 require(['jquery', 'bootstrap','login','slide'], function () {
+    $(document).ready(function () {
+        var x = getUrlParam('x');
+        var y = getUrlParam('y');
+        console.log(x,y)
+        
+    })
+    function getUrlParam(name){
+        var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+        var r = window.location.search.substr(1).match(reg);
+        if (r!=null) return unescape(r[2]); return null;
+    }
+   
 
    
 });
